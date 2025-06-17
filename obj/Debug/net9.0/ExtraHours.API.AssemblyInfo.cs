dbee5a81f5b472d653542639f3f162dffa2fa2e6
Vero-nica22 <mcveronica66@gmail.com>
2025-06-16 21:54:11 -0500

    Agrego readme

--- obj/Debug/net9.0/ExtraHours.API.AssemblyInfo.cs
+++ obj/Debug/net9.0/ExtraHours.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExtraHours.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16123acb39febf9b410b86fa0b2f3116c9f00538")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5acb856ca5093212103feff46d31752ffea557e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExtraHours.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExtraHours.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
